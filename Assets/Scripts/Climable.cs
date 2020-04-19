7432f049eba364b644201f566373c83153dbb98a
Sjors <sjorsv11@gmail.com>
2020-04-19 21:10:31 +0200

    Pressureplate
    + start vines

---
+++ Assets/Scripts/Climable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Climable : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+    // bool climbing = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        // climbing = true;
+        if (Input.GetKeyDown(KeyCode.W) == true) //& climbing == true 
+                player.transform.position += new Vector3(0, 5, 0);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //climbing = false;
+        player.transform.position += new Vector3(0, -4, 0);
+    }
+}
---
+++ Assets/Scripts/Pressureplate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pressureplate : MonoBehaviour
+{
+    [SerializeField]
+    GameObject door;
+    private void OnTriggerEnter(Collider other)
+    {
+        door.transform.position += new Vector3(0, 4, 0);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        door.transform.position += new Vector3(0, -4, 0);
+    }
+}
